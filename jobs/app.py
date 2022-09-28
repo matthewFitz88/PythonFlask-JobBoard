43e8310e95c147eb8fb5779a515e5abacd611176
matthewFitz88 <matthewfitzgerald@mail.weber.edu>
2022-09-27 22:02:47 -0600

    changes again

--- jobs/app.py
+++ jobs/app.py
@@ -1,1 +1,6 @@
-from flask import Flask render_template = Flask(__name__)
+from flask import Flask render_template() = Flask(__name__)
+Flask app(__name__)
+
+def jobs():
+    return render_template('index.html')
+route(/jobs)
